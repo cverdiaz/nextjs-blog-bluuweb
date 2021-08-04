cf0d049bc2fccceb8d635825f6e9e6110631540c
cverdiaz <carlos.vera@umag.cl>
2021-08-04 20:07:23 -0300

    5 - getStaticProps(Consumiendo una API)

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,9 +1,36 @@
 import Layout from '../../components/Layout';
 
-export default function index() {
+export default function index({data}) {
     return (
         <Layout title = 'about | nex.js' description="agregue descripcion">
             <h1>Lista de Posts</h1>
+            {
+                data.map(({id, title, body}) => (
+                    <div key={id}>
+                        <h3>{id} - {title}</h3>
+                        <p>{body}</p>
+                    </div>
+                ))
+            }
         </Layout>
     )
 }
+
+/** esta funcion solo se va a ejecutar en ele servidor */
+export async function getStaticProps(){
+
+    try{
+  
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await res.json();
+  
+      return {
+        props: {
+          data
+        }
+      }
+  
+    } catch(error){
+      console.log(error);
+    }
+  }
